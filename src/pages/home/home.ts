c5f908176ff00b1b9689493337a10af1f83adb62
zakareeya <Szakareeya@ptnicec.ac.th>
2018-11-08 14:42:25 +0700

    send project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public AcSheet: ActionSheetController,
+    public navCtrl: NavController) {
 
+  }
+
+  ShowAction(){
+    const action = this.AcSheet.create({
+      title: 'เลื่อกรายการ',
+      subTitle:'-------------------------------',
+      buttons:[
+        {
+          text: 'Add'
+        },
+        {
+          text: 'Edit',
+          role: 'edit',
+          handler: () => {
+            console.log('แก้ไขแล้ว');
+        }
+        },
+        {
+          text: 'Delete',
+          role: 'delete',
+          handler: () => {
+            console.log('ลบเรียบร้อยแล้ว');
+        }
+        }
+     ]
+    });
+    action.present();
   }
 
 }
